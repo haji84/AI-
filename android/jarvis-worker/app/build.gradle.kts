110481664f7156cf49ae65c5cae9efc238b04b7b
haji84 <qq.1130034@gmail.com>
2026-09-13 17:18:42 +0900

    chore: bump JARVIS Worker to 0.2.0 for self-update

--- android/jarvis-worker/app/build.gradle.kts
+++ android/jarvis-worker/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "ai.jarvis.worker"
         minSdk = 28
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
