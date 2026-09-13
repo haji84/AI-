edb1183436e92c12c435d32cb39a696e4703b1d9
haji84 <qq.1130034@gmail.com>
2026-09-14 01:44:33 +0900

    chore: bump Android Worker to 0.3.4

--- android/jarvis-worker/app/build.gradle.kts
+++ android/jarvis-worker/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "ai.jarvis.worker"
         minSdk = 28
         targetSdk = 37
-        versionCode = 7
-        versionName = "0.3.3"
+        versionCode = 8
+        versionName = "0.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
