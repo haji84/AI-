88735d132a610f728e1d8405c02d82a24129d171
haji84 <qq.1130034@gmail.com>
2026-09-16 21:32:30 +0900

    feat: expose separate speech persona and style controls

--- src/app/jarvis/mobile/voice/page.tsx
+++ src/app/jarvis/mobile/voice/page.tsx
@@ -1,9 +1,15 @@
 import MobileVoiceCommander from "./MobileVoiceCommander";
+import SpeechPersonaStylePanel from "./SpeechPersonaStylePanel";
 import "../mobile.css";
 import "./voice.css";
 
 export const dynamic = "force-dynamic";
 
 export default function JarvisMobileVoicePage() {
-  return <MobileVoiceCommander />;
+  return (
+    <>
+      <SpeechPersonaStylePanel />
+      <MobileVoiceCommander />
+    </>
+  );
 }
