b4ebd2a3003dcfc407cef13a470a7998dbb5c797
haji84 <qq.1130034@gmail.com>
2026-09-16 15:06:26 +0900

    style: load JARVIS primary shell styles

--- src/app/jarvis/layout.tsx
+++ src/app/jarvis/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import type { ReactNode } from "react";
 import JarvisPrimaryShell from "./JarvisPrimaryShell";
 import "./jarvis.css";
+import "./shell.css";
 
 export const metadata: Metadata = {
   title: "JARVIS Commander",
