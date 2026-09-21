12f0e0ec998ac3a90aea10f726e42d7d37ef0bab
haji84 <qq.1130034@gmail.com>
2026-09-22 08:19:17 +0900

    fix: replace literal backslash-n sequences in JarvisWorkShell.tsx

--- src/app/jarvis/JarvisWorkShell.tsx
+++ src/app/jarvis/JarvisWorkShell.tsx
@@ -1,4 +1,5 @@
-"use client";\nimport "./work-shell.css";
+"use client";
+import "./work-shell.css";
 import { useEffect, useState } from "react";
 import { JARVIS_THEMES, jarvisTheme, type JarvisThemeId } from "./theme-catalog";
 
