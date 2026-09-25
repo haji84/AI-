de5f172d627ded747caf6d2350f3d6079d6e400d
haji84 <qq.1130034@gmail.com>
2026-09-25 01:02:08 -0700

    test(owner): scaffold Google OIDC enrollment tests

---
+++ tests/google-owner-oidc.test.ts
@@ -0,0 +1,6 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+
+test("Google Owner OIDC implementation is added through the approved #1218 branch", () => {
+  assert.equal(true, true);
+});
